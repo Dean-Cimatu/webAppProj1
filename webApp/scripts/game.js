c7afb7e8703a1554dc88932c9db866c8717ca900
Dean-Cimatu <deancimatu@gmail.com>
2025-10-21 15:27:06 +0100

    Start function for phaser

--- webApp/scripts/game.js
+++ webApp/scripts/game.js
@@ -1,1 +1,14 @@
-//phsaer code comes here
+let player;
+let cursors;
+
+const config = {
+    type: phaser.Auto,
+    width: 800,
+    height: 600,
+    backgroundColor: '#7CFC00',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
